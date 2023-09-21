062c3cf77dcad941c5abd5b12fce77d82cd5cd98
Koraus <dmytro.kashchynets@gmail.com>
2023-09-21 18:18:34 +0200

    solved the problem of checking for an undefined  when checking the placement of a figure

--- src/check-match.tsx
+++ src/check-match.tsx
@@ -61,13 +61,29 @@ export const canPlaceFigureInCoords = (
     figure: Figure, field: number[][], coords: [number, number]
 ) => {
 
+    const maxHeightIndex = field.length - 1;
+    const maxWidthIndex = field[0].length - 1;
+
     const [x, y] = coords;
     let canPlace = true;
 
     for (let i = 0; i < figure?.length; i++) {
         for (let j = 0; j < figure[i].length; j++) {
-            if (field[x + i][y + j] !== 0 && figure[i][j] !== 0
-                || x + i > 9 || y + j > 9 || x + i < 0 || y + j < 0) {
+
+            if (field[x + i] === undefined) {
+                canPlace = false
+                console.log('can`t Place')
+                return
+            }
+            if (
+                field[x + i][y + j] !== 0
+                && figure[i][j] !== 0
+                || field[x + i][y + j] === undefined
+                || x + i > maxHeightIndex
+                || y + j > maxWidthIndex
+                || x + i < 0
+                || y + j < 0) {
+
                 canPlace = false
                 console.log('can`t Place')
                 return
