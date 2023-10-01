4239d121f5202adbe98ed98e0b4cac832d0e7c20
ndry <ndry@users.noreply.github.com>
2023-10-01 12:51:27 +0300

    added exlint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+  env: { browser: true, es2022: true },
+  ignorePatterns: ['/*', '!/src', '!/backend'],
+  plugins: [
+    "@typescript-eslint",
+    // "react", 
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  overrides: [],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
   },
-}
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { "allowConstantExport": true },
+    ],
+    // "linebreak-style": ["warn", "unix"], // enforced by git
+    "quotes": ["warn", "double"],
+    "semi": ["warn", "always"],
+    "max-nested-callbacks": ["warn", 2],
+    "comma-dangle": ["warn", "always-multiline"],
+    "no-warning-comments": ["warn"],
+    "no-console": ["warn", { "allow": ["warn", "error"] }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      "argsIgnorePattern": "^_",
+      "varsIgnorePattern": "^_",
+      "caughtErrorsIgnorePattern": "^_"
+    }],
+  },
+};
