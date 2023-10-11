428630719719c0b27a3327c1bbbe2e9c2165b6b8
Koraus <dmytro.kashchynets@gmail.com>
2023-10-11 16:41:23 +0200

    add forest decoration

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,6 +5,7 @@ import { Sound } from "./level/sound.tsx";
 import audioUrl from "../assetsx/put-figure.wav";
 import { OverlayGui as _OverlayGui } from "./overlay-gui.tsx";
 import { memo } from "react";
+import { Forest } from "../assetsx/forest.tsx";
 
 const PlayingField = memo(_PlayingField);
 const GameFiguresArea = memo(_GameFiguresArea);
@@ -26,6 +27,35 @@ export default function App() {
                 camera={{ fov: 35, position: [5, 20, -10] }}
                 gl={{ useLegacyLights: true }}
             >
+                {
+                    Array.from(
+                        { length: 10 },
+                        () => Array.from(
+                            { length: 11 },
+                            () => 0))
+                        .map(
+                            (v, i) => {
+                                return v.map(
+                                    (val, j) => {
+                                        if (i < 7 && i > 1 && j < 8 && j > 1) {
+                                            return null;
+                                        } else {
+                                            const key = `${i}-${j}`;
+                                            return <Forest
+                                                key={key}
+                                                position={[
+                                                    (j * 2.4) - 11,
+                                                    0,
+                                                    (i * 2.4) - 5
+                                                ]}
+                                                scale={[0.8, 0.8, 0.8]}
+                                                rotation={[0, (Math.PI / 2) * i, 0]}
+                                            />
+                                        }
+                                    }
+                                )
+                            })
+                }
                 <Sound url={audioUrl} />
                 <ambientLight intensity={0.5} />
                 <pointLight position={[10, 15, -5]} intensity={0.8} />
