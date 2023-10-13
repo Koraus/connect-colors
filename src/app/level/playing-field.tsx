cf874cae8429a4e7b4c932ffd10345569ee27390
ndry <ndry@users.noreply.github.com>
2023-10-13 16:47:53 +0300

    minor refactor

--- src/app/level/playing-field.tsx
+++ src/app/level/playing-field.tsx
@@ -3,13 +3,11 @@ import { FieldCell } from "./field-cell";
 import { levelInitRecoil } from "./level-recoil";
 
 export const PlayingField = () => {
-    const li = useRecoilValue(levelInitRecoil);
-    const m = li.fieldMap.length;
-    const n = li.fieldMap[0].length;
+    const { fieldMap } = useRecoilValue(levelInitRecoil);
 
-    return <>
-        {Array.from({ length: n }, (_, i) =>
-            Array.from({ length: m }, (_, j) =>
-                <FieldCell key={i * m + j} coords={[i, j]} />))}
-    </>;
+    return <>{
+        fieldMap.map((row, i) =>
+            row.map((_, j) =>
+                <FieldCell key={`${i}_${j}`} coords={[i, j]} />))
+    }</>;
 };
