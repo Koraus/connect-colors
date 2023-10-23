a53bd599f085cfda763f5a54a1aa8604104748d1
Koraus <dmytro.kashchynets@gmail.com>
2023-10-23 15:08:03 +0200

    added progress visualization

--- src/app/progress-bar.tsx
+++ src/app/progress-bar.tsx
@@ -6,30 +6,48 @@ import { levelRecoil } from "./level/level-recoil";
 export const ProgressBar = () => {
     const level = useRecoilValue(levelRecoil);
     return (
-        <div style={{
+        <div css={{
             boxSizing: "border-box",
             borderRadius: "1.5vmax",
             border: "0.3vmax solid #E4E4E4",
-            background: "linear-gradient(90deg, #49B5F7 0%, #2578CF 100%)",
+            background: "gray",
             padding: "0.8vmax",
             width: "21vmax",
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
             fontWeight: "bold",
-            fontSize: "3vmax",
+            fontSize: "2.6vmax",
             color: "#E4E4E4",
             position: "fixed",
             right: "2vmax",
             bottom: "2vmax",
         }}>
-            <span> {level.state.figureStockLeft} </span> <span> / {level.state.level.figureStock}</span>
-            <img
-                draggable={false}
-                src={icoPath}
-                style={{ width: "9vmax" }}
-                alt="figures"
-            />
+            <div css={{
+                posiiton: "reletive",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "spaceEvenly",
+            }}>
+                <img
+                    draggable={false}
+                    src={icoPath}
+                    style={{ width: "9vmax", marginRight: "1vmax" }}
+                    alt="figures"
+                />
+                <span> {level.state.level.figureStock - level.state.figureStockLeft}&nbsp; </span>
+                <span> / {level.state.level.figureStock}</span>
+
+                <div css={{
+                    zIndex: -1,
+                    borderRadius: "1vmax",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    background: "linear-gradient(90deg, #49B5F7 0%, #2578CF 100%)",
+                    width: `${(level.state.level.figureStock - level.state.figureStockLeft) / level.state.level.figureStock * 100}%`,
+                    height: "100%",
+                }}> </div>
+
+            </div>
+
         </div>
     )
 }
